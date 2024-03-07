57c72ac8637b62e9a6e461a06c25287e02fdd668
Ринат <rinatmuhamedyarov1967@gmail.com>
2024-03-07 15:26:43 +0500

    Создан черновик

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создание массива объектов
+        object[] array = new object[] { "Hello", 2, "world", ":-)" };
+
+        // Вывод элементов массива
+        foreach (var item in array)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
