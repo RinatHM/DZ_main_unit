b94930144dbd67da69415010227ecabcfb08d85f
Ринат <rinatmuhamedyarov1967@gmail.com>
2024-03-11 14:36:52 +0500

    Add Program fail

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
-﻿using System;
-
-class Program
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+//int Length(string[] array);
+int CountNewMassiv = 0;
+for (int i = 0; i < array.Length; i++)
 {
-    static void Main()
-    {
-        // Создание массива объектов
-        object[] array = new object[] { "Hello", 2, "world", ":-)" };
-
-        // Вывод элементов массива
-        foreach (var item in array)
+    if (array[i].Length <= 3)
         {
-            Console.WriteLine(item);
+        CountNewMassiv++;
         }
-    }
+}
+string [] ResArray = new string [CountNewMassiv];
+int CountIndex = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        {
+        ResArray[CountIndex] = array[i];
+        Console.WriteLine(ResArray[CountIndex]);
+        CountIndex++;
+        }
 }
